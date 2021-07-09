bbb2d3949c38cafefc822bceb1267df04b958abb
super-rogatory <chukwudi10@gmail.com>
2021-07-08 23:42:39 -0700

    Fixed react-redux bug

--- reduxutil/src/store.js
+++ reduxutil/src/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from 'react-thunk';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [thunk]; 
 
 const store  = createStore(rootReducer, initialState, applyMiddleware(...middleware));
 
